a41a7e97b5613d68dba9f6e85c6f78533fc63cd6
rides1 <rpd35@njit.edu>
2024-06-10 22:48:00 -0400

    components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-
+import React from "react";
+import Header from "./components/Header";
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+    <Header/>
+
+    </>
   );
 }
 
