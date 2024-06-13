0d85c8ce03c0f13c0bb91e0dde13be40ab129bb9
rides1 <rpd35@njit.edu>
2024-06-12 22:23:16 -0400

    Components done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 import React from "react";
 import Header from "./components/Header";
+import Figure from "./components/Figure";
+import WrongLetters from "./components/WrongLetters";
+import Word from "./components/Word";
+import Popup from "./components/Popup";
+import Notify from "./components/Notify";
+
+
 function App() {
   return (
     <>
     <Header/>
-
+    <div classname= "game-container">
+      <Figure/>
+      <WrongLetters/>
+      <Word/>
+      <Popup/>
+      <Notify/>
+    </div>
     </>
   );
 }
---
+++ src/components/Notify.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notify = () => {
+  return (
+    <div class="notification-container" id="notification-container">
+    <p>You have already entered this letter</p>
+  </div>
+    )
+}
+
+export default Notify
